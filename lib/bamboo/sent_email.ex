16098ec7bed39f4e65e251d4d302f39d048ec5fc
Paul Smith <paulcsmith0218@gmail.com>
2016-03-25 16:47:40 -0400

    Improve LocalAdapter docs

--- lib/bamboo/sent_email.ex
+++ lib/bamboo/sent_email.ex
@@ -3,9 +3,9 @@ defmodule Bamboo.SentEmail do
   Used for storing and retrieving sent emails when used with Bamboo.LocalAdapter
 
   When emails are sent with the Bamboo.LocalAdapter, they are stored in
-  Bamboo.SentEmail. Use the following function to store and retrieve the emails.
-  Remember to start the Bamboo app by adding it to the app list in mix.exs or
-  starting it with Application.ensure_all_started(:bamboo)
+  Bamboo.SentEmail. Use the functions in this module to store and retrieve the emails.
+  Remember to start the Bamboo app by adding it to the app list in `mix.exs` or
+  starting it with `Application.ensure_all_started(:bamboo)`
   """
 
   @id_length 16
