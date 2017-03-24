38d084e47619b9882b8b8dea2592d33c1d007372
Jason Draper <jason@drapergeek.com>
2017-03-24 13:19:15 -0400

    Move the config into the task

--- lib/mix/start_email_previewer_task.ex
+++ lib/mix/start_email_previewer_task.ex
@@ -1,13 +1,14 @@
 defmodule Mix.Tasks.Bamboo.StartEmailPreviewer do
   use Mix.Task
-
+  @port 4004
   @shortdoc "Start the email preview server"
   def run(_) do
     Mix.Task.run "app.start"
+    Application.put_env(:bamboo, :email_preview_module, __MODULE__)
     {:ok, _} = Application.ensure_all_started(:cowboy)
-    Plug.Adapters.Cowboy.http Bamboo.EmailPreviewPlug, [], port: 4003
+    Plug.Adapters.Cowboy.http Bamboo.EmailPreviewPlug, [], port: @port
 
-    IO.puts "Running email preview on port 4003"
+    IO.puts "Running email preview on port #{@port}"
     no_halt()
   end
 
@@ -17,5 +18,23 @@ defmodule Mix.Tasks.Bamboo.StartEmailPreviewer do
 
   defp iex_running? do
     Code.ensure_loaded?(IEx) && IEx.started?
+  end
+
+  def previews do
+    [
+      %{
+        path: "customer_email",
+        name: "Customer Email",
+        email: fn ->
+          %{html_body: "Hi Customer HTML", text_body: "Hi Customer Text"}
+        end,
+      }, %{
+        path: "guest_email",
+        name: "Guest Email",
+        email: fn ->
+          %{html_body: "Hi Guest HTML", text_body: "Hi Guest Text"}
+        end,
+      },
+    ]
   end
 end
