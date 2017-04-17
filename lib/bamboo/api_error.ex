0a0244bea79e7033c3d831ca0c208bc4966b766b
Joao Bernardo <jb.amaro95@gmail.com>
2017-04-17 16:18:59 +0100

    Adds raise_api_error documentation

--- lib/bamboo/api_error.ex
+++ lib/bamboo/api_error.ex
@@ -5,6 +5,40 @@ defmodule Bamboo.ApiError do
 
   defexception [:message]
 
+  @doc """
+  Raises an `ApiError` with the given `message` or `service_name`, `response` and `params`.
+
+  An extra error message can be added using a fourth parameter `extra_message`.
+
+  ## Examples
+
+      iex> raise_api_error("Error message")
+      ** (Bamboo.ApiError) Error Message
+
+      iex> raise_api_error(service_name, response, params)
+      ** (Bamboo.ApiError) There was a problem sending the email through the <service_name> API.
+
+      Here is the response:
+
+      "<response>"
+
+      Here are the params we sent:
+
+      "<params>"
+
+      iex> raise_api_error(service_name, response, params, extra_message)
+      ** (Bamboo.ApiError) There was a problem sending the email through the <service_name> API.
+
+      Here is the response:
+
+      "<response>"
+
+      Here are the params we sent:
+
+      "<params>"
+
+      <extra_message>
+  """
   def raise_api_error(message), do: raise(__MODULE__, message: message)
   def raise_api_error(service_name, response, params, extra_message \\ "") do
     message = """
@@ -17,9 +51,13 @@ defmodule Bamboo.ApiError do
     Here are the params we sent:
 
     #{inspect params, limit: :infinity}
-    
-    #{extra_message}
     """
+
+    message =
+      case extra_message do
+        "" -> message
+        em -> message <> "\n#{em}\n"
+      end
 
     raise(__MODULE__, message: message)
   end
