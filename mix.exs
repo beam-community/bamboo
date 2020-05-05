342005d5f943778049b79b03ed20830a09043fbd
May Miller-Ricci <maymillerricci@gmail.com>
2020-05-05 14:13:05 -0700

    Update version and changelog for version 1.5.0

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Bamboo.Mixfile do
   def project do
     [
       app: :bamboo,
-      version: "1.4.0",
+      version: "1.5.0",
       elixir: "~> 1.2",
       source_url: @project_url,
       homepage_url: @project_url,
