32ba772d98308a5151c50dd17d100fa27b9bb59c
German Velasco <germsvel@gmail.com>
2020-12-29 15:53:31 -0500

    Introduce Bamboo.Template and Bamboo.View
    What changed?
    =============
    
    We introduce two new modules `Bamboo.Template` and `Bamboo.View` so that
    Bamboo users can use HTML and text templates without having to rely on
    Phoenix (not all Bamboo users use Phoenix).
    
    The implementation is based heavily on Phoenix's Template and View
    modules. So a huge thanks to them for all the hard work.
    
    Example usage:
    
    ```elixir
    defmodule MyApp.EmailView do
      use Bamboo.View, path: "/path/to/this/view/templates"
    end
    
    defmodule MyApp.Email do
      use Bamboo.Template, view: MyApp.EmailView
    
      def welcome_email do
        new_email()
        |> put_layout({MyApp.EmailLayout, :email})
        |> render(:welcome_email)
      end
    end
    ```
    
    The goal is for `Bamboo.Template` to coexist with `Bamboo.Phoenix`, and
    `Bamboo.Phoenix` can be moved to a separate package in the future.
    
    Difference from Phoenix Views
    -----------------------------
    
    When using `Bamboo.View` we have to specify the full path where the
    templates for that view will live (instead of the root path as is done
    in `Phoenix.View`.
    
    We could do fancier things where we auto detect the location based on
    the module name, but that seems a little magical and perhaps unnecessary
    for now. We can always add that if we need it. For now, we can specify a
    view, and set the path for its templates.
    
    Difference from Bamboo.Phoenix
    -----------------------------
    
    Unlike `Bamboo.Phoenix`, `Bamboo.Template` allows for overriding the
    view with `put_view/2` for cases when we don't have to rely on the view
    defined in `use Bamboo.Template, view: EmailView`.
    
    Note on alternate implementation
    --------------------------------
    
    The first pass at doing this used`EEx.function_from_file` instead
    `EEx.compile_file` and unquoting the compiled file as we define the
    function. But I ran into issues with the `function_from_file` approach
    because assigns needed to be keyword lists instead of maps. At that
    point, I looked at the approach `Phoenix.Template` uses, and decided to
    follow their approach of using `EEx.compile_file`.
    
    See [Phoenix.Template's approach]:
    https://github.com/phoenixframework/phoenix/blob/e05af0ad5527c9a192de122f3e43d7ed4c7a4c9f/lib/phoenix/template.ex#L346-L365
    
    Note on updated Bamboo.Phoenix tests
    ------------------------------------
    
    Even though there were no changes to `Bamboo.Phoenix`, there were a
    couple of changes to `bamboo/phoenix_test`. That happened because we
    tried to reuse as many as the test templates as possible.
    
    We did separate a `PhoenixLayoutView` since the layouts render their
    contents differently. `Bamboo.Phoenix` uses a version of Phoenix that
    still uses `render @view_module, @view_template, assigns` in the layout.
    But `Bamboo.View` is using `@inner_content`, like more recent versions
    of Phoenix.
    
    We also have to include the `function_in_view/0` function in
    `bamboo/phoenix_test`'s `EmailView` since the view compiles a new
    template that calls that function.

---
+++ lib/bamboo/view.ex
@@ -0,0 +1,201 @@
+defmodule Bamboo.View do
+  @moduledoc """
+  Compiles and renders templates defined in a given path.
+
+  Functions defined in the view are available to use in its templates.
+
+  ## Example
+
+      defmodule MyApp.EmailView do
+        use Bamboo.View, path: "/lib/my_app/email/templates"
+
+        def app_title do
+          "My Super App"
+        end
+      end
+
+      # lib/my_app/email_templates/welcome.html
+      <h1>Welcome to <%= app_title() %></h1>
+  """
+
+  defmodule UndefinedTemplateError do
+    @moduledoc """
+    Exception raised when a template cannot be found.
+    """
+    defexception [:module, :template, :available]
+
+    def message(exception) do
+      ~s"""
+      Could not render #{inspect(exception.template)} for #{inspect(exception.module)}
+
+      #{available_templates(exception.available)}
+      """
+    end
+
+    defp available_templates([]), do: "No templates were compiled for this module."
+
+    defp available_templates(available) do
+      "The following templates were compiled:\n\n" <>
+        Enum.map_join(available, "\n", &"* #{&1}") <>
+        "\n"
+    end
+  end
+
+  defmacro __using__(path: path) do
+    quote do
+      @templates_path unquote(path)
+      @before_compile unquote(__MODULE__)
+    end
+  end
+
+  defmacro __using__(opts) do
+    raise ArgumentError, """
+    expected Bamboo.View to have a path set, instead got: #{inspect(opts)}.
+
+    Please set a path where this view's template are defined e.g. use Bamboo.View, path: "/lib/my_app/email/templates/account"
+    """
+  end
+
+  defmacro __before_compile__(env) do
+    templates_path = Module.get_attribute(env.module, :templates_path)
+
+    results =
+      templates_path
+      |> templates_to_compile()
+      |> Enum.map(&template_and_name/1)
+      |> Enum.map(fn {template, name} ->
+        {name, compile(template, name)}
+      end)
+
+    template_names = Enum.map(results, &elem(&1, 0))
+    render_functions = Enum.map(results, &elem(&1, 1))
+
+    quote do
+      unquote(render_functions)
+
+      def render_template(template, _assigns) do
+        raise UndefinedTemplateError,
+          module: __MODULE__,
+          template: template,
+          available: unquote(template_names)
+      end
+    end
+  end
+
+  @doc false
+  def render_email(view, email, template, assigns) do
+    email
+    |> put_defaults(view)
+    |> merge_assigns(assigns)
+    |> render(template)
+  end
+
+  defp put_defaults(%{private: private} = email, view_module) do
+    private =
+      private
+      |> Map.put_new(:html_layout, false)
+      |> Map.put_new(:text_layout, false)
+      |> Map.put_new(:view_module, view_module)
+
+    %{email | private: private}
+  end
+
+  defp merge_assigns(%{assigns: email_assigns} = email, assigns) do
+    assigns = email_assigns |> Map.merge(Enum.into(assigns, %{}))
+    email |> Map.put(:assigns, assigns)
+  end
+
+  defp render(email, template) when is_atom(template) do
+    render_html_and_text_emails(email, template)
+  end
+
+  defp render(email, template) do
+    render_text_or_html_email(email, template)
+  end
+
+  defp render_html_and_text_emails(email, template) do
+    view_template = Atom.to_string(template)
+
+    email
+    |> Map.put(:html_body, render_html(email, view_template <> ".html"))
+    |> Map.put(:text_body, render_text(email, view_template <> ".text"))
+  end
+
+  defp render_text_or_html_email(email, template) do
+    cond do
+      String.ends_with?(template, ".html") ->
+        email |> Map.put(:html_body, render_html(email, template))
+
+      String.ends_with?(template, ".text") ->
+        email |> Map.put(:text_body, render_text(email, template))
+
+      true ->
+        raise ArgumentError, """
+        Template name must end in either ".html" or ".text". Template name was #{
+          inspect(template)
+        }
+
+        If you would like to render both and html and text template,
+        use an atom without an extension instead.
+        """
+    end
+  end
+
+  defp render_html(email, template) do
+    layout = email.private.html_layout
+    render_within_layout(layout, email, template)
+  end
+
+  defp render_text(email, template) do
+    layout = email.private.text_layout
+    render_within_layout(layout, email, template)
+  end
+
+  defp render_within_layout(_layout = false, email, template) do
+    module = email.private.view_module
+    module.render_template(template, email.assigns)
+  end
+
+  defp render_within_layout({layout_view, layout_template}, email, template) do
+    module = email.private.view_module
+    contents = module.render_template(template, email.assigns)
+
+    assigns =
+      email.assigns
+      |> Map.put(:inner_content, contents)
+
+    layout_view.render_template(layout_template, assigns)
+  end
+
+  defp templates_to_compile(directory) do
+    Path.wildcard(directory <> "/*.eex")
+  end
+
+  defp template_and_name(template) do
+    name =
+      template
+      |> Path.basename()
+      |> Path.rootname(".eex")
+
+    {template, name}
+  end
+
+  defp compile(template, name) do
+    quoted_contents = EEx.compile_file(template, line: 1, engine: EEx.SmartEngine)
+    function_name = String.to_atom(name)
+
+    quote do
+      @file unquote(name)
+      @external_resource unquote(template)
+
+      defp unquote(function_name)(var!(assigns)) do
+        _ = var!(assigns)
+        unquote(quoted_contents)
+      end
+
+      def render_template(unquote(name), assigns) do
+        unquote(function_name)(assigns)
+      end
+    end
+  end
+end
