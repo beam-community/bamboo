6b9d3eb4d4c53ec9475927ef9cf97818d0ada9da
Daniel McMorrow <sesa736249@se.com>
2025-03-10 16:57:14 +0000

    plug cowboy fix

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule Bamboo.Mixfile do
     ]
   end
 
-  defp elixirc_paths(:test), do: elixirc_paths() ++ ["test/support"]
+  defp elixirc_paths(:test), do: elixirc_paths() ++ ["test/support", "lib_dev"]
+  defp elixirc_paths(:dev), do: elixirc_paths() ++ ["lib_dev"]
   defp elixirc_paths(_), do: elixirc_paths()
   defp elixirc_paths, do: ["lib"]
 
